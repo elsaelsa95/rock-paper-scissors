881cfe397f84181d3d93b933750677af0941da51
Elsa Lustiana <elsa.lustiana@gmail.com>
2023-03-22 12:55:27 +0700

    done

---
+++ play/src/utils/data.ts
@@ -0,0 +1,24 @@
+export interface RandomIcon {
+    id: number
+    name: string
+    image: string
+}
+
+const data: RandomIcon[] = [
+    {
+        id: 1,
+        name: "scissors",
+        image: "https://www.clipartmax.com/png/middle/428-4288836_your-hand-hand-clipart-png-rock-paper-scissors.png"
+    },
+    {
+        id: 2,
+        name: "rock",
+        image: "https://image.pngaaa.com/783/3313783-middle.png"
+    },
+    {
+        id: 3,
+        name: "paper",
+        image: "https://flyclipart.com/thumb2/play-rock-paper-scissors-with-us-246506.png"
+    }
+]
+export default data
